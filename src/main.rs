80bb9809bee43a4cbe1c3448debdd992aedc0096
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-25 18:19:23 +0800

    Add response to HTTP request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,11 +8,12 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200\n");
             }
             Err(e) => {
                 println!("error: {}", e);
