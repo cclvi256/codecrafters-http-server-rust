3fff22960018068ece36bc6754599070b56be6b2
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-27 16:26:36 +0800

    Add concurrency support

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                handle_connection(_stream);
+                std::thread::spawn(|| handle_connection(_stream));
             }
             Err(e) => {
                 println!("error: {}", e);
