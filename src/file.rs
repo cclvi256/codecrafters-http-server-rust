958d286c9bab0b5612a2de1980273cf4262dfdf6
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-08 21:37:37 +0800

    refactor: Re-ordered the file structure

--- src/file.rs
+++ src/file.rs
@@ -1,7 +1,14 @@
-pub fn read_file(env_path: &str, path: &str) -> Result<String, std::io::Error> {
-    todo!()
+use std::{fs::File, io::Write};
+
+pub fn read_file(env_path: &str, path: &str) -> Result<Vec<u8>, std::io::Error> {
+    let full_path = format!("{}/{}", env_path, path);
+    let content = std::fs::read(full_path)?;
+    Ok(content)
 }
 
-pub fn write_file(env_path: &str, path: &str, content: &str) -> Result<(), std::io::Error> {
-    todo!()
+pub fn write_file(env_path: &str, path: &str, content: &Vec<u8>) -> Result<(), std::io::Error> {
+    let full_path = format!("{}/{}", env_path, path);
+    let mut file = File::open(full_path)?;
+    file.write_all(content.as_slice())?;
+    Ok(())  
 }
